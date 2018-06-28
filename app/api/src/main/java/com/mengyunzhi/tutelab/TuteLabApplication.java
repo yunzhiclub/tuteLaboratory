42fa869e1890d99def0a0b4267bbfb890e87daf4
panjie <3792535@qq.com>
2018-06-28 09:30:17 +0800

    后台初始化

---
+++ app/api/src/main/java/com/mengyunzhi/tutelab/TuteLabApplication.java
@@ -0,0 +1,12 @@
+package com.mengyunzhi.tutelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuteLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuteLabApplication.class, args);
+	}
+}
